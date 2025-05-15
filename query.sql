6081f4afda2ed3603736d7b33c20df0d04baccfa
kaimorquecho <kaimorquecho@ucsb.edu>
2025-05-15 12:42:36 -0700

    added a check for years covered per state in cleaning script, modified query, started data viz

--- query.sql
+++ query.sql
@@ -1,16 +1,10 @@
 
 -- Question 1: From 2020 and 2022, what were top 10 counties with the highest number of asthma emergency visits? 
 
-SELECT a.state, a.county, a.year, q.max_aqi, a.er_visits
+SELECT a.county,a.year, a.er_visits, q.max_aqi
 FROM asthma_emergencies a
-JOIN annual_aqi q ON a.state = q.state AND a.county = q.county AND a.year = q.year
-WHERE a.state = 'California'
-ORDER BY q.max_aqi DESC
-LIMIT 10;
-
-SELECT a.state, a.county, a.year, q.max_aqi, a.er_visits, 
-FROM asthma_emergencies a
-JOIN annual_aqi q ON a.state = q.state AND a.county = q.county AND a.year = q.year
-WHERE a.state = 'California'
-ORDER BY a.er_visits DESC
-LIMIT 10;
+JOIN annual_aqi q
+  ON a.state = q.state 
+ AND a.county = q.county 
+ AND a.year = q.year
+WHERE a.state = 'California';
